e80ea6a5b78945a24beeb2312e3815c050f1fa77
Ethan Pineda <joshuapineda66@gmail.com>
2024-12-05 19:32:58 -0600

    changes

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to your Express server" });
+  res.json({ message: "changed api messaged" });
 });
 
 const PORT = process.env.PORT || 3005;
